68c71cd8833f01ac768ac8f01eab1fe9e96c7942
pauline <pauline.feytel@gmail.com>
2019-08-21 10:06:47 +0200

    Add state for tasks

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
 import Board from '../Board/Board';
@@ -6,13 +6,14 @@ import CreateTask from '../CreateTask/CreateTask';
 import DeletedTasks from '../DeletedTasks/DeletedTasks';
 
 function App() {
+    const [tasks, setTasks] = useState([]);
     return (
         <BrowserRouter>
             <NavBar />
-            <Route path='/board' component={Board} />
-            <Route path='/task/create' component={CreateTask} />
-            <Route path='/deleted-tasks' component={DeletedTasks} />
+            <Route path='/board' render={(props) => <Board {...props} tasks={tasks} component={Board} />} />
+            <Route path='/task/create' render={(props) => <CreateTask {...props} tasks={tasks} setTasks={setTasks} component={CreateTask} />} />
+            <Route path='/deleted-tasks' render={(props) => <DeletedTasks {...props} tasks={tasks} component={DeletedTasks} />} />
         </BrowserRouter>
     )
 }
-export default App
+export default App;
